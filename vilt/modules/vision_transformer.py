dc3c181dbae6e1005e5b9bee65562e4cb0ee77a3
Mustafa Talha Ilerisoy <m.t.ilerisoy@student.tue.nl>
2025-01-26 14:36:37 +0100

    remove unused line problem for quantization

--- vilt/modules/vision_transformer.py
+++ vilt/modules/vision_transformer.py
@@ -555,7 +555,7 @@ class VisionTransformer(nn.Module):
         return feats, labels
 
     def visual_embed(self, _x, max_image_len=200, mask_it=False):
-        _, _, ph, pw = self.patch_embed.proj.weight.shape
+        # _, _, ph, pw = self.patch_embed.proj.weight.shape
 
         x = self.patch_embed(_x)
         x_mask = (_x.sum(dim=1) != 0).float()[:, None, :, :]
