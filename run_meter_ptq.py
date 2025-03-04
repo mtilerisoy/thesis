96838f17a137c89563a585b011c46325ccba562e
mtilerisoy <m.t.ilerisoy@student.tue.nl>
2025-03-04 06:01:34 +0100

    update ptq scripts to use quant_utils

--- run_meter_ptq.py
+++ run_meter_ptq.py
@@ -1,7 +1,7 @@
 import os
 # Limit the number of CPUs
-os.environ["OMP_NUM_THREADS"] = "8"  # Set this to the number of CPUs you want to use
-os.environ["MKL_NUM_THREADS"] = "8"  # Set this to the number of CPUs you want to use
+os.environ["OMP_NUM_THREADS"] = "10"  # Set this to the number of CPUs you want to use
+os.environ["MKL_NUM_THREADS"] = "10"  # Set this to the number of CPUs you want to use
 
 import copy
 import pytorch_lightning as pl
@@ -19,7 +19,7 @@ resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
 import time
 import torch
 from torch.utils.data import Subset
-import dynamic_quantization as dq
+from quantization_utils import quantize_modules
 from torch.utils.data import Subset
 
 class SmallMTDataModuleVILT(MTDataModule):
@@ -81,38 +81,8 @@ def main(_config):
 
     max_steps = _config["max_steps"] if _config["max_steps"] is not None else None
 
-    # trainer = pl.Trainer(
-    #     accelerator=_config["accelerator"],
-    #     devices=_config["num_gpus"],
-    #     num_nodes=_config["num_nodes"],
-    #     precision=_config["precision"],
-    #     strategy="ddp",
-    #     benchmark=True,
-    #     deterministic=True,
-    #     max_epochs=_config["max_epoch"] if max_steps is None else 1000,
-    #     max_steps=max_steps,
-    #     callbacks=callbacks,
-    #     logger=logger,
-    #     # prepare_data_per_node=False,
-    #     # replace_sampler_ddp=False,
-    #     accumulate_grad_batches=grad_steps,
-    #     log_every_n_steps=10,
-    #     # flush_logs_every_n_steps=10,
-    #     # resume_from_checkpoint=_config["resume_from"],
-    #     # weights_summary="top",
-    #     fast_dev_run=_config["fast_dev_run"],
-    #     val_check_interval=_config["val_check_interval"],
-    # )
-
-    # if not _config["test_only"]:
-    #     print(f"Training model")
-    #     trainer.fit(model, datamodule=dm)
-    # else:
-    #     print(f"Testing model")
-    #     trainer.test(model, datamodule=dm)
-
     # =============== Testing QUANTIZED Model ===============
-    trainer_cpu = pl.Trainer(
+    trainer = pl.Trainer(
         accelerator="cpu",
         devices=1,
         num_nodes=_config["num_nodes"],
@@ -130,80 +100,24 @@ def main(_config):
         val_check_interval=_config["val_check_interval"],
     )
 
-    # # =============== Testing HALF Model ===============
-    # trainer_half = pl.Trainer(
-    #     accelerator="cpu",
-    #     devices=1,
-    #     num_nodes=_config["num_nodes"],
-    #     precision=16,
-    #     strategy="ddp",
-    #     benchmark=True,
-    #     deterministic=False,
-    #     max_epochs=_config["max_epoch"] if max_steps is None else 1000,
-    #     max_steps=max_steps,
-    #     callbacks=callbacks,
-    #     logger=logger,
-    #     # accumulate_grad_batches=grad_steps,
-    #     log_every_n_steps=10,
-    #     fast_dev_run=_config["fast_dev_run"],
-    #     val_check_interval=_config["val_check_interval"],
-    # )
-
-
-    # # ========== Testing Full Precision Model ==========
-    # print("========== Testing Full Precision Model - FP32 ==========")
-    # start_time_fp32 = time.time()
-    # trainer_cpu.test(model, datamodule=dm)
-    # end_time_fp32 = time.time()
-    # print("Time taken for FP32 Inference: ", end_time_fp32 - start_time_fp32)
-
-
-    # # ========== Testing Half Precision Model ==========
-    # print("========== Testing Half Precision Model - FP16 ==========")
-    # model_half = copy.deepcopy(model)
-    # model_half = model_half.half()
-
-    # start_time_fp16 = time.time()
-    # trainer_half.test(model_half, datamodule=dm)
-    # end_time_fp16 = time.time()
+    # ======= Define Modules to Quantize ========
+    modules_to_quantize = [
+    "text_transformer.encoder.layer.2.output.dense",
+    "text_transformer.encoder.layer.2.intermediate.dense"
+    # "text_transformer.encoder.layer.3.output.dense",
+    # "text_transformer.encoder.layer.3.intermediate.dense"
+    ]
 
 
     # ========== Testing Quantized Model ==========
     print("========== Testing Quantized Model - INT8 ==========")
-    model_int8 = dq.quantize_model_dynamic(model, 8)
+    model_dynamic = quantize_modules(model, modules_to_quantize, 4)
+    print(f"Model size after quantization:")
+    print_size_of_model(model_dynamic)
 
-    start_time_int8 = time.time()
-    trainer_cpu.test(model_int8, datamodule=dm)
-    end_time_int8 = time.time()
-    print("Time taken for STATIC INT8 Inference: ", end_time_int8 - start_time_int8)
-
-
-    # ========== Testing Quantized Model ==========
-    print("========== Testing Quantized Model - INT4 ==========")
-    model_int4 = dq.quantize_model_dynamic(model, 4)
-
-    start_time_int4 = time.time()
-    trainer_cpu.test(model_int4, datamodule=dm)
-    end_time_int4 = time.time()
-    print("Time taken for DYNAMIC INT4 Inference: ", end_time_int4 - start_time_int4)
-
-
-    # # ========== Testing Quantized Model ==========
-    # print("========== INT8 DEFAULT !!!! QUANT ==========")
-    # _model = copy.deepcopy(model)
-    # torch.quantization.quantize_dynamic(
-    #         _model, {torch.nn.Embedding}, dtype=torch.quint8, inplace=True
-    #     )
-    # torch.quantization.quantize_dynamic(
-    #         _model, {torch.nn.Linear, torch.nn.LayerNorm}, dtype=torch.qint8, inplace=True
-    #     )
-
-    # start_time_int8 = time.time()
-    # trainer_cpu.test(_model, datamodule=dm)
-    # end_time_int8 = time.time()
-    # print("Time taken for DYNAMIC INT8 DEfAULT Inference: ", end_time_int8 - start_time_int8)
-
-    
-    # print("Time taken for FP16 Inference: ", end_time_fp16 - start_time_fp16)
+    start_time = time.time()
+    trainer.test(model_dynamic, datamodule=dm)
+    end_time = time.time()
+    print("Time taken for DYNAMIC INT4 Inference: ", end_time - start_time)
     
     
