a06c1cdb6d5c5a30d48def12c836ea99fd679afe
mtilerisoy <m.t.ilerisoy@student.tue.nl>
2025-03-08 13:22:11 +0100

    implmenet tensorboard logging | use train split while training

--- run_vilt_kd.py
+++ run_vilt_kd.py
@@ -26,20 +26,24 @@ import argparse
 # Create an ArgumentParser object
 parser = argparse.ArgumentParser(description="Custom QAT Script")
 parser.add_argument("-e", "--epochs",           type=int,   default=2,                  help="Number of epochs to train the model")
-parser.add_argument("-l", "--learning_rate",    type=float, default=2e-4,               help="Learning rate for the optimizer")
+parser.add_argument("-l", "--learning_rate",    type=float, default=1e-5,               help="Learning rate for the optimizer")
 parser.add_argument("-d", "--dataset",          type=str,   default="nlvr2_ood",        help="Dataset to train the model on")
-parser.add_argument("-p", "--percentage",       type=float, default=0.01,                help="Percentage of the dataset to use for fine-tuning")
-parser.add_argument("-g", "--gpu",              type=int,   default=1,                  help="List of GPUs to use for training")
+parser.add_argument("-p", "--percentage",       type=float, default=0.5,               help="Percentage of the dataset to use for fine-tuning")
+parser.add_argument("-g", "--gpu",              type=int,   default=[0,1],                  help="List of GPUs to use for training")
 parser.add_argument("-a", "--alpha_kd",         type=float, default=0.5,                help="Alpha value for the KD loss")
 parser.add_argument("-t", "--temperature",      type=float, default=1.0,                help="Temperature value for the KD loss")
-parser.add_argument("-L", "--log_dir",          type=str,   default="logs",             help="Directory to store the logs")
+parser.add_argument("-L", "--log_dir",          type=str,   default="experiments/logs",      help="Directory to store the logs")
+parser.add_argument("-x", "--exp_name",         type=str,   default="kd_loss_compare",  help="Name of the experiment")
+parser.add_argument("-s", "--max_steps",        type=int,   default=25000,              help="Maximum number of steps to train the model")
 args = parser.parse_args()
 print("┌─────────────────────────────────────────────────────────────────────────────────────────────────────┐")
 print("│                                                                                                     │")
 print(f"│  Running with: epochs={args.epochs}, learning_rate={args.learning_rate}, percentage={args.percentage}    \n│")
 print(f"│  dataset={args.dataset}, gpu={args.gpu}, alpha_kd={args.alpha_kd}, temperature={args.temperature}    \n│")
-print(f"│  log_dir={args.log_dir}                                                                             \n│")
+print(f"│  log_dir={args.log_dir}, max_steps={args.max_steps}                                                                          \n│")
 print("│                                                                                                     │")
+if args.epochs == -1:
+    print("│  Running INFINTE Training Loop. Please stop the script manually.                                    │")
 print("└─────────────────────────────────────────────────────────────────────────────────────────────────────┘")
 
 
@@ -79,10 +83,7 @@ if __name__ == "__main__":
         raise ValueError(f"Unknown dataset: {args.dataset}")
     
     # ========== Update the configuration ==========
-    _config["exp_name"] = "kd_loss_test"
-    os.makedirs(_config["exp_name"], exist_ok=True)
-    _config["log_dir"] = os.path.join(_config["exp_name"], args.log_dir)
-    _config["batch_size"] = 32
+    _config["batch_size"] = 128
     _config["per_gpu_batchsize"] = 16
     _config = copy.deepcopy(_config)
     pl.seed_everything(_config["seed"])
@@ -90,13 +91,12 @@ if __name__ == "__main__":
     # ========== Initialize the datamodule for pl.Trainer ==========
     # dm = MTDataModule(_config, dist=False)
     dm = SmallMTDataModuleVILT(_config, dist=False, percentage=args.percentage)
-    dm.setup("test", is_random=True)
-    # train_dataloader = dm.train_dataloader()
+    dm.setup("", is_random=True)
+    train_dataloader = dm.train_dataloader()
     # val_dataloader = dm.val_dataloader()
-    train_dataloader = dm.test_dataloader()
+    # test_dataloader = dm.test_dataloader()
 
-    # print("Val Dataloader Length: ", len(val_dataloader))
-    print("Test Dataloader Length: ", len(train_dataloader))
+    print("Dataloader Length: ", len(train_dataloader))
 
     print(f"Length of the first batch: {len(next(iter(train_dataloader))['answers'])}")
     print(f"Shape of the first batch: {next(iter(train_dataloader))['image_0'][0].shape}")
@@ -126,23 +126,27 @@ if __name__ == "__main__":
 
     print("Model Scale Factor: ", model_student.scale_factor)
     # ========== Initialize the trainer for full precision ==========
+    _config["exp_name"] = args.exp_name
+    _config["log_dir"] = args.log_dir
     exp_name = f'{_config["exp_name"]}'
-
     os.makedirs(_config["log_dir"], exist_ok=True)
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        save_top_k=1,
-        verbose=True,
-        monitor="val/the_metric",
-        mode="max",
-        save_last=True,
-    )
+
     logger = pl.loggers.TensorBoardLogger(
         _config["log_dir"],
-        name=f'{exp_name}_seed{_config["seed"]}_from_{_config["load_path"].split("/")[-1][:-5]}',
+        name=f'{exp_name}_alpha{args.alpha_kd}_lr{args.learning_rate}_epochs{args.epochs}_steps{args.max_steps}_kd_layer0_from_{_config["load_path"].split("/")[-1][:-5]}',
+        default_hp_metric=False
     )
 
-    lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
-    callbacks = [checkpoint_callback, lr_callback]
+    # checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    #     save_top_k=1,
+    #     verbose=True,
+    #     monitor="val/the_metric",
+    #     mode="max",
+    #     save_last=True,
+    # )
+    
+    # lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
+    # callbacks = [checkpoint_callback, lr_callback]
 
     num_gpus = (
         _config["num_gpus"]
@@ -159,14 +163,16 @@ if __name__ == "__main__":
     # =============== Testing Quantized Model ===============
     trainer = pl.Trainer(
         accelerator="gpu",
-        devices=[args.gpu],
+        devices=2,
         num_nodes=_config["num_nodes"],
         precision=_config["precision"],
         benchmark=True,
         deterministic=True,
         max_epochs=args.epochs,
-        max_steps=50000,
-        logger=False,
+        max_steps=args.max_steps, # 25000,
+        logger=logger,
+        # callbacks=lr_callback,
+        log_every_n_steps=1,
         accumulate_grad_batches=grad_steps,
         enable_checkpointing=False,
         fast_dev_run=_config["fast_dev_run"],
@@ -197,6 +203,8 @@ if __name__ == "__main__":
     print(f"Min, Max and Mean of the QAT WEIGHTS: \n{torch.min(fc2_weight_after_dyn_quant)}, {torch.max(fc2_weight_after_dyn_quant)}")
     print("Model Scale Factor after training: \n", model_student.scale_factor)
     print("============================================================")
+
+    raise ValueError("Stopping the script here after QAT before inference")
 
 
 
