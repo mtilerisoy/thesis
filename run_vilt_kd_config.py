0ad3aee2e14e022cf0f969ed24adbc39dccd5334
mtilerisoy <m.t.ilerisoy@student.tue.nl>
2025-04-14 17:34:45 +0200

    implement on single gpu

--- run_vilt_kd_config.py
+++ run_vilt_kd_config.py
@@ -3,21 +3,21 @@ current_time = datetime.now().strftime("%Y%m%d_%H%M")
 
 EPOCHS = 3
 MAX_STEPS = -1
-LEARNING_RATE = 0.01 #1e-5
+LEARNING_RATE = 0.0001 #1e-5
 
 DATASET = "nlvr2_original"
-PERCENTAGE = 0.5
-GPU = [0,1]
+PERCENTAGE = 1
+GPU = [1]
 
-ALPHA_KD = 0.5
+ALPHA_KD = 0
 KD_LAYER = 0
 TEMPERATURE = 1.0
 
-modules_to_train = {'layer_names': ["scale_factor",
+modules_to_train = {'layer_names': [#"scale_factor",
                                         f"transformer.blocks.{KD_LAYER}.mlp.fc1",
                                         f"transformer.blocks.{KD_LAYER}.mlp.fc2"],
                         'kd_layer': KD_LAYER}
 
 
 LOG_DIR = "experiments/logs"
-EXP_NAME = f"{current_time}_SCALE_kd_{EPOCHS}_{MAX_STEPS}_{LEARNING_RATE}_{ALPHA_KD}_{KD_LAYER}_{DATASET}_{PERCENTAGE}"
+EXP_NAME = f"xxx{current_time}_QAT_only_log_CLS_lr{LEARNING_RATE}"
